4d43fa7da30625bb580564ec63089c8b54f6c85b
Gavin King <plumpnation@gmail.com>
2021-04-01 13:10:02 +0200

    initial commit

---
+++ src/main/java/com/plumpnation/ReflectionExperiment.java
@@ -0,0 +1,4 @@
+package com.plumpnation;
+
+public class ReflectionExperiment {
+}
---
+++ src/test/java/com/plumpnation/ReflectionExperimentTest.java
@@ -0,0 +1,13 @@
+package com.plumpnation;
+
+import org.junit.jupiter.api.Test;
+import java.lang.reflect.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReflectionExperimentTest {
+    @Test
+    void testSomething() {
+        assertEquals(1, 1);
+    }
+}
