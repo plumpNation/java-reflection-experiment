faf4c955ce92cfe72613ce93fb748a1a73804136
Gavin King <plumpnation@gmail.com>
2021-04-01 13:40:57 +0200

    Added example for reading class field names

---
+++ src/main/java/com/plumpnation/Person.java
@@ -0,0 +1,7 @@
+package com.plumpnation;
+
+public class Person {
+    private String name;
+    private int age;
+    public int yearOfBirth;
+}
--- src/test/java/com/plumpnation/ReflectionExperimentTest.java
+++ src/test/java/com/plumpnation/ReflectionExperimentTest.java
@@ -2,12 +2,36 @@ package com.plumpnation;
 
 import org.junit.jupiter.api.Test;
 import java.lang.reflect.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ReflectionExperimentTest {
     @Test
-    void testSomething() {
-        assertEquals(1, 1);
+    void testFindPersonAllFields() {
+        Object something = new Person();
+
+        Field[] declaredFields = something.getClass().getDeclaredFields();
+        List<String> declaredFieldNames = getFieldNames(declaredFields);
+
+        assertEquals(3, declaredFieldNames.size());
+        assertTrue(declaredFieldNames.containsAll(Arrays.asList("name", "age", "yearOfBirth")));
+    }
+
+    @Test
+    void testFindPersonPublicFields() {
+        Object something = new Person();
+
+        Field[] fields = something.getClass().getFields();
+        List<String> fieldNames = getFieldNames(fields);
+
+        assertEquals(1, fieldNames.size());
+        assertTrue(fieldNames.contains("yearOfBirth"));
+    }
+
+    private List<String> getFieldNames(Field[] fields) {
+        return Arrays.stream(fields).map(Field::getName).collect(Collectors.toList());
     }
 }
